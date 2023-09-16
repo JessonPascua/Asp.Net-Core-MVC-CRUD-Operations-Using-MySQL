10691e370a7c56b7939a1081bfc47c8c7335296a
JessonWork <jessonpascua.work@gmail.com>
2023-09-16 16:54:47 +0800

    Add project files.

---
+++ Asp.Net-Core-MVC-CRUD-Operations-Using-MySQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.Net_Core_MVC_CRUD_Operations_Using_MySQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
